2e8f3d8f1ed2d7eed852e1d1830ccf417b142bb4
段书勇 <oncwnuIYb9d6_lK14X4ABpGJTLt0@git.weixin.qq.com>
2019-11-10 10:59:40 +0800

    init

--- src/main/java/com/will/cross/Application.java
+++ src/main/java/com/will/cross/Application.java
@@ -1,16 +1,13 @@
 package com.will.cross;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
 
 /**
  * @author PualrDwade
  * springboot启动项目
  */
-
 @SpringBootApplication
 public class Application {
     public static void main(String[] args) {
