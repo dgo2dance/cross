7c3ccf181e3241b4cfb375d75154d5d68b74b1a5
段书勇 <oncwnuIYb9d6_lK14X4ABpGJTLt0@git.weixin.qq.com>
2019-11-16 12:16:42 +0800

    add for hellllo

--- src/main/java/com/will/cross/configurer/WebMvcConfigurer.java
+++ src/main/java/com/will/cross/configurer/WebMvcConfigurer.java
@@ -109,7 +109,7 @@ public class WebMvcConfigurer extends WebMvcConfigurerAdapter {
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
         //接口签名认证拦截器，该签名认证比较简单，后期项目可以使用Json Web Token或其他更好的方式替代。
-        if (!"dev".equals(env)) { //开发环境忽略签名认证
+        if (!"dev".equals(env)||!"prod".equals(env)) { //开发环境忽略签名认证
             registry.addInterceptor(new HandlerInterceptorAdapter() {
                 @Override
                 public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
