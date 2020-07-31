063180267c6468a05d063efa3848f14f01931278
duanshuyong <duanshuyong0@163.com>
2020-07-31 16:16:03 +0800

    mmm

--- src/main/java/com/will/cross/controller/ScheduleSaleController.java
+++ src/main/java/com/will/cross/controller/ScheduleSaleController.java
@@ -19,10 +19,34 @@ import java.util.List;
 public class ScheduleSaleController {
     @Resource
     private ScheduleSaleService scheduleSaleService;
-
+	
+	
+	// 录入销售数据；
     @RequestMapping(value = "/addSale", method = RequestMethod.POST, produces = "application/json")
-    public Result add(@RequestBody ScheduleSale scheduleSale) {
-        scheduleSaleService.save(scheduleSale);
+    public Result add(@RequestBody ScheduleSaleDTO scheduleSale) {
+		
+		ScheduleSale sv= new ScheduleSale();
+
+		// 循环设置sale数据，并保存
+		for(int i=0;i<=scheduleSale.getTableDate.split(,)){
+			
+			sv.setId(UUID.randomUUID().toString());
+			sv.setAmount();
+            sv.setBeginDate();
+            sv.setEndDate();
+            sv.setType();
+            sv.setCreateBy(getOpenId());
+            sv.setCreateDate(new Date());
+            sv.setUpdateBy(getOpenId());
+            sv.setUpdateDate(new Date());
+            sv.setMaster(getMasterId());
+			
+			i=i+25;
+			
+			scheduleSaleService.save(scheduleSale);
+
+		}
+		
         return ResultGenerator.genSuccessResult();
     }
 
@@ -43,12 +67,24 @@ public class ScheduleSaleController {
         ScheduleSale scheduleSale = scheduleSaleService.findById(id);
         return ResultGenerator.genSuccessResult(scheduleSale);
     }
-
+	
+	
+    // 查询数据并展示
     @GetMapping
     public Result list(@RequestParam(defaultValue = "0") Integer page, @RequestParam(defaultValue = "0") Integer size) {
-        PageHelper.startPage(page, size);
+     
+
+             //  PageHelper.startPage(page, size);
+        Condition query=new Condition(ScheduleSale.class);
+        String masterid=getMasterId();
+        query.createCriteria().andEqualTo("master",masterid);
+        List<ScheduleShift> list = scheduleShiftService.findByCondition(query);
+
         List<ScheduleSale> list = scheduleSaleService.findAll();
         PageInfo pageInfo = new PageInfo(list);
         return ResultGenerator.genSuccessResult(pageInfo);
+
+
+
     }
 }
---
+++ src/main/java/com/will/cross/dto/ScheduleSaleDTO.java
@@ -0,0 +1,21 @@
+package com.will.cross.dto;
+
+import com.will.cross.model.ScheduleTimeTable;
+
+import java.util.List;
+
+public class ScheduleTableDTO {
+
+    /*
+    日期
+     */
+    private String date;
+
+
+    /*
+        单元板内容
+         */
+    private  String tableData;
+
+
+}
